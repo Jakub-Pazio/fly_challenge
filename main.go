7f5e65d88e61980e4fafffae8a7a9b67c8665fd3
Jakub-Pazio <19jakubpazio99@gmail.com>
2023-04-10 00:26:34 +0200

    version with unique ids generated with node id and local unique id

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"strconv"
+	"strings"
+	"sync"
 
-	uuid "github.com/google/uuid"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
 func main() {
 	n := maelstrom.NewNode()
-	// idGen := IdGenerator{currentId: 1}
+	idGen := IdGenerator{currentId: 1}
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
@@ -28,12 +30,13 @@ func main() {
 
 	n.Handle("generate", func(msg maelstrom.Message) error {
 		var body map[string]any
+		nodeId := msg.Dest
 		if err := json.Unmarshal(msg.Body, &body); err != nil {
 			return err
 		}
 
 		body["type"] = "generate_ok"
-		body["id"] = uuid.New().String()
+		body["id"] = idGen.generateGlobalId(nodeId)
 
 		return n.Reply(msg, body)
 	})
@@ -41,4 +44,23 @@ func main() {
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type IdGenerator struct {
+	mu        sync.Mutex
+	currentId uint64
+}
+
+func (i *IdGenerator) generateLocalId() string {
+	i.mu.Lock()
+	defer i.mu.Unlock()
+	i.currentId++
+	return strconv.FormatUint(i.currentId, 10)
+}
+
+func (i *IdGenerator) generateGlobalId(nodeId string) string {
+	builder := strings.Builder{}
+	builder.WriteString(nodeId)
+	builder.WriteString(i.generateLocalId())
+	return builder.String()
 }
