f18594b40a88456e960e20a9e9ce9f9135f1aaec
Jakub-Pazio <19jakubpazio99@gmail.com>
2023-04-10 13:49:31 +0200

    wrote just enough code to finish 3a

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 	idGen := IdGenerator{currentId: 1}
+	localNumbers := LocalNumbers{numList: make(map[float64]struct{})}
 
 	n.Handle("echo", func(msg maelstrom.Message) error {
 		// Unmarshal the message body as an loosely-typed map.
@@ -41,6 +42,42 @@ func main() {
 		return n.Reply(msg, body)
 	})
 
+	n.Handle("broadcast", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+		number := body["message"].(float64)
+		localNumbers.addNumber(number)
+
+		body["type"] = "broadcast_ok"
+		delete(body, "message")
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("read", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["messages"] = localNumbers.getNumbers()
+		body["type"] = "read_ok"
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "topology_ok"
+		delete(body, "topology")
+		return n.Reply(msg, body)
+	})
+
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
@@ -63,4 +100,26 @@ func (i *IdGenerator) generateGlobalId(nodeId string) string {
 	builder.WriteString(nodeId)
 	builder.WriteString(i.generateLocalId())
 	return builder.String()
+}
+
+type LocalNumbers struct {
+	mu      sync.Mutex
+	numList map[float64]struct{}
+}
+
+func (l *LocalNumbers) addNumber(number float64) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+
+	l.numList[number] = struct{}{}
+}
+
+func (l *LocalNumbers) getNumbers() []float64 {
+	l.mu.Lock()
+	result := make([]float64, len(l.numList))
+	defer l.mu.Unlock()
+	for elem := range l.numList {
+		result = append(result, elem)
+	}
+	return result
 }
